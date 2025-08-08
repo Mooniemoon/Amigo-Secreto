f731564d0fe0f0d1f3bd1024c4c5846a0d073439
Ian <iangame97@gmail.com>
2025-08-07 20:36:48 -0600

    Se agregaron las funciones para: agregar un amigo al array, limpiar la caja cuando se agrego el nombre, actualizar la lista que se muestra en pantalla y para sortear el amigo.

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
 let nombreAmigos = [];
 
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById("amigo").value;
+    let amigo = nombreAmigo.trim();
+    if (amigo != ""){
+    nombreAmigos.push(amigo);
+    limpiarCaja();
+    actualizarListaAmigos();
+ 
+    }else {
+        alert("Por favor agrega un nombre valido.");
+    }
+}
 
+function limpiarCaja(){
+    document.getElementById("amigo").value = "";
+}
+
+function actualizarListaAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (i = 0; i < nombreAmigos.length; i++){
+        lista.innerHTML += `<li>${nombreAmigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo(){
+    if (nombreAmigos.length > 0){
+        let sorteo = Math.floor(Math.random() * nombreAmigos.length);
+        let sorteado = nombreAmigos[sorteo];
+        document.getElementById("resultado").innerHTML = sorteado;
+        nombreAmigos.splice(sorteo, 1);
+        actualizarListaAmigos();
+    }else {
+        alert("No hay más amigos para sortear.");
+    }
+}
 
